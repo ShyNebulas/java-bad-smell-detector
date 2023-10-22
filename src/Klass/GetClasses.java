4a247316fe953294c81d5a3a89ded3f1dfea98ee
Cameron SE2020 <cameron.bell.2020@uni.strath.ac.uk>
2023-10-22 20:22:54 +0100

    refactor: renamed GetKlasss and refactored code

--- src/Klass/GetClasses.java
+++ src/Klass/GetClasses.java
@@ -3,13 +3,12 @@ package Klass;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
-public class GetKlasss extends VoidVisitorAdapter<List<Class<?>>> {
+public class GetClasses extends VoidVisitorAdapter<List<Class<?>>> {
     @Override
     public void visit(ClassOrInterfaceDeclaration klass, List<Class<?>> collector) {
-        List<Class<?>> klasses = new ArrayList<>(List.of(klass.getClass().getClasses()));
-        collector.addAll(klasses);
+        List<Class<?>> children = List.of(klass.getClass().getClasses());
+        collector.addAll(children);
         super.visit(klass, collector);
     }
 }
