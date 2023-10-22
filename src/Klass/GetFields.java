8f0bcd7b5e574ffbe210c7b5b34c1c806ab3d8ef
Cameron SE2020 <cameron.bell.2020@uni.strath.ac.uk>
2023-10-22 20:27:54 +0100

    refactor: GetFields consistency

--- src/Klass/GetFields.java
+++ src/Klass/GetFields.java
@@ -2,14 +2,24 @@ package Klass;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class GetFields extends VoidVisitorAdapter<List<FieldDeclaration>> {
-    @Override
-    public void visit(ClassOrInterfaceDeclaration klass, List<FieldDeclaration> collector) {
-        super.visit(klass, collector);
-        collector.addAll(klass.getFields());
+public class GetFields {
+    private static class GetFieldsVisitor extends VoidVisitorAdapter<List<FieldDeclaration>> {
+        @Override
+        public void visit(ClassOrInterfaceDeclaration klass, List<FieldDeclaration> collector) {
+            super.visit(klass, collector);
+            collector.addAll(klass.getFields());
+        }
+    }
+    List<FieldDeclaration> getValues(ClassOrInterfaceDeclaration klass) {
+        List<FieldDeclaration> fields = new ArrayList<>();
+        VoidVisitor<List<FieldDeclaration>> fieldsVisitor = new GetFieldsVisitor();
+        fieldsVisitor.visit(klass, fields);
+        return fields;
     }
 }
